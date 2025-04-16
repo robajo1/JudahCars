f8afcf34108fea69d918e44675749ce161d7cb9c
Nahom <nahokifl@gmail.com>
2025-04-16 21:01:10 +0300

    cards css

--- src/components/ProductsComponents/ProductsBody.jsx
+++ src/components/ProductsComponents/ProductsBody.jsx
@@ -148,7 +148,7 @@ function ProductsBody() {
                 <img src="/images/fuel.png" alt="fuel" style={{height:'25px', width:'auto'}}/>
                 <img src="/images/trans.png" alt="trans" style={{width:'30px'}}/>
               </div>
-              <div className="vehicle-description" style={{display:'flex'}}>
+              <div className="vehicle-description" style={{display:'flex', gap:'20px'}}>
                 <p >
                   {car.mileage.toLocaleString()} KM     
                 </p>
